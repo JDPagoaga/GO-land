f65ab1b93e8f16236c472d87c2a58d63628b51d5
Jairon Pagoaga <donay_91@live.com>
2023-03-18 11:22:40 -0600

    code using var keyword and default inicialize

--- GoLand/shortExpression.go
+++ GoLand/shortExpression.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+//var keyword allow to declare variables out from main function
+//be careful to declare variables in this way , it allow to inicialize but
+//don't check the use
+var m = "using var keyword"
+
+// inicialize with default value
+var b int
+var c string
+var flag bool
+
 func main() {
 	// with X : , declared the variable X
 	// with =2 , assigned a value to variable X
@@ -17,5 +27,9 @@ func main() {
 	fmt.Println(y)
 	z := "It's a string"
 	fmt.Println(z)
+	fmt.Println(m)
+	fmt.Println("default value of int", b)
+	fmt.Println("default value of string*", c, "*")
+	fmt.Println("default value of boolean", flag)
 
 }
