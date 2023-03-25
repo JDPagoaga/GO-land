240b307bc32ff6ab1b365236d1ac2536eb6acaff
Jairon Pagoaga <donay_91@live.com>
2023-03-25 13:22:30 -0600

    add example using conversions with own types

--- GoLand/ownType.go
+++ GoLand/ownType.go
@@ -16,14 +16,27 @@ var i Cont
 func main() {
 	fmt.Println("using several types")
 	fmt.Println("print p ", p)
+	fmt.Printf("%T\n", p)
 	fmt.Println("print y ", y)
+	fmt.Printf("%T\n", y)
 	fmt.Println("print z ", z)
+	fmt.Printf("%T\n", z)
 	fmt.Println("print n ", n)
+	fmt.Printf("%T\n", n)
 	fmt.Println("print i ", i)
+	fmt.Printf("%T\n", i)
 
 	y = true
-	//this action onli can be, b'cause they are the same type
+	//this action only can be, b'cause they are the same type
 	z = y
 	fmt.Println("print z ", z)
+	// using conversions
+	fmt.Println("using conversions ")
+	p = bool(n)
+	fmt.Println("new value of p", p)
+	p = bool(y)
+	fmt.Println("next value of p", p)
+	z = flagY(n)
+	fmt.Println("new  value of z", z)
 
 }
